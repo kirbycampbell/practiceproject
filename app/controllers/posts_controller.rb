8408bfd99cc84b2c7d19e690ece6d8e75d375c0b
Kirby <jkirbycampbell@gmail.com>
2018-09-26 11:09:17 -0700

    Continuing set up
    Continuing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   def index
-
+    p = Post.new
+    p.title = "suckerFIsh"
+    p.content = "your fucked brah"
+    p.save
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new(params)
+  end
+
 end
