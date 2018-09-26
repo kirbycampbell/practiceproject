2a3132caffc26e8dd86d268957a8a31c6cfc077f
Kirby <jkirbycampbell@gmail.com>
2018-09-26 11:19:28 -0700

    Initial Testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    u = User.create(:username => "kingfuckbutt")
     p = Post.new
     p.title = "suckerFIsh"
     p.content = "your fucked brah"
+    p.user_id = 1
     p.save
     @posts = Post.all
   end
