abbf68fac0574551f4b2d9b8d796788426c4b4af
Kirby <jkirbycampbell@gmail.com>
2018-10-01 19:01:56 -0700

    Changed create to new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.new(post_params)
     if @post.save
       redirect_to post_url(@post)
     else
